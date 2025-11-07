79df0895f4ce0478ae009107d790cec8eace1f39
hugoattal <4563971+hugoattal@users.noreply.github.com>
2025-11-08 00:34:16 +0100

    Add tailwind import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-export default function tailwind() {
-    console.log("tailwind");
-}
+import { makePlugin } from "@luna-park/plugin";
 
-export { default as PHelloWorld } from "./components/PHelloWorld.vue";
+import icon from "./logo.svg";
+
+export default makePlugin({
+    id: "tailwind",
+    name: "Tailwind",
+    description: "",
+    icon,
+    inject: {
+        js: ({ mode }) => {
+            if (mode === "editor") {
+                return `import tailwind from "@luna-park/tailwind-scope"; tailwind({scope: { from: "[data-root]" }});`;
+            }
+        }
+    }
+});
